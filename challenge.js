3a2ae49892928a582e0e46999411e0e687fa8827
Tremayne Johnson <tremayne4@gmail.com>
2023-10-10 10:53:08 -0400

    challenge

---
+++ challenge.js
@@ -0,0 +1,7 @@
+const videos = ['cats', 'news', 'games', 'sports']
+
+for (let i = 0; i < videos.length; i++){
+
+console.log(videos[i])
+
+}
---
+++ loops.js
@@ -0,0 +1,16 @@
+// say hello 3 times
+
+//for(let i = 0; i < 3;i++){
+  //  console.log("hello")
+
+//const people = ['todd', 'andrew', 'derek']
+//r (let i = 0; i < people.length;i++){
+  //  console.log ('hello ' + people[i])
+
+const fruits =['apple', 'grape', 'banana']
+for(let i = < array.length-1; i >= 0; i--){
+    console.log (fruits[i])
+
+}
+
+
